1eae9f91d9300069aee9655b093aaed3708bd82b
Zakhar Aleshkin <z.aleshkin@dodobrands.io>
2026-03-24 00:44:56 +0300

    compute for initial state

--- task_01/src/features/RenderingUseState/RenderingUseState.tsx
+++ task_01/src/features/RenderingUseState/RenderingUseState.tsx
@@ -1,15 +1,39 @@
 import { useState } from 'react';
 
 export const RenderingUseState = (): JSX.Element => {
-	const [count, setCount] = useState(0);
+	const getInitialCount = (): number => {
+		let a = 0;
+		let b = 0;
+		let accum = 0;
+		for (let i = 0; i < 100_000_000; i++) {
+			a = i;
+			b = 100_000_000 - i;
+			accum = accum + a + b;
+		}
+
+		console.log('rerender, accum: ', accum);
+
+		return 0;
+	};
+
+	// bad way... or useMemo - это тоже не будет вызывать повтора вычисления
+	// const [count, setCount] = useState(getInitialCount());
+	const [count, setCount] = useState(() => getInitialCount());
 
 	const handleClick = (): void => {
-		// eslint-disable-next-line no-console
 		console.log('rerender, count = ', count);
+
 		// setCount(count + 1); // bad way
 		// setCount(count + 1); // bad way
+		// setTimeout(() => {
+		// 	setCount(count + 1); // bad way
+		// }, 1000);
+
 		setCount(prev => prev + 1);
 		setCount(prev => prev + 1);
+		setTimeout(() => {
+			setCount(prev => prev + 1);
+		}, 1000);
 	};
 
 	return (
