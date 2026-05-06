ca18152c8a5fc18c36200f7d1e2ae4f8937ea43f
Zakhar Aleshkin <z.aleshkin@dodobrands.io>
2026-05-07 01:25:29 +0300

    remove unused api and types

--- task_03/src/app/store/index.ts
+++ task_03/src/app/store/index.ts
@@ -2,17 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { ordersApi } from 'entities/order/api';
 import { ordersReducer } from 'entities/order/model/ordersSlice';
-import { stationsApi } from 'entities/station/api';
 import { stationFilterReducer } from 'features/stationFilter/model/slice';
 
 export const store = configureStore({
 	reducer: {
 		[ordersApi.reducerPath]: ordersApi.reducer,
-		[stationsApi.reducerPath]: stationsApi.reducer,
 		orders: ordersReducer,
 		stationFilter: stationFilterReducer,
 	},
-	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(ordersApi.middleware, stationsApi.middleware),
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(ordersApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- task_03/src/entities/station/model/types.ts
+++
@@ -1,7 +0,0 @@
-import { type StationType } from 'shared/types/domain';
-
-export interface Station {
-	id: string;
-	name: string;
-	type: StationType;
-}
